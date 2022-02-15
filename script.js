d4f837d604da5185e5ff98fdcdb0f4b7430d3eec
Daniel <meskal1@rambler.ru>
2022-02-15 22:27:21 +0300

    задачa_22

--- script.js
+++ script.js
@@ -204,3 +204,31 @@ function century(year) {
 // function array_diff(a, b) {
 // 	return a.filter(e => !b.includes(e));
 // }
+
+// Вы, наверное, знаете систему «лайков» из Facebook и других страниц. Люди могут «лайкать» сообщения в блогах, изображения или другие элементы. Мы хотим создать текст, который должен отображаться рядом с таким элементом.
+// Реализуйте функцию, которая принимает массив, содержащий имена людей, которым понравился элемент. Он должен возвращать отображаемый текст, как показано в примерах:
+// []                                -->  "no one likes this"
+// ["Peter"]                         -->  "Peter likes this"
+// ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+// ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+// ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+// function likes(names) {
+// 	switch (names.length) {
+// 		case 0: return `no one likes this`;
+// 		case 1: return `${names[0]} likes this`;
+// 		case 2: return `${names[0] + ` and ` + names[1]} like this`;
+// 		case 3: return `${names[0] + `, ` + names[1] + ` and ` + names[2]} like this`;
+// 		default:
+// 			return `${names[0] + `, ` + names[1] + ` and ` + (names.length - 2)} others like this`;
+// 	}
+// }
+// console.log(likes(["Alex", "Jacob", "Mark", "Max"]));
+// function likes(names) {
+// 	return {
+// 		0: 'no one likes this',
+// 		1: `${names[0]} likes this`,
+// 		2: `${names[0]} and ${names[1]} like this`,
+// 		3: `${names[0]}, ${names[1]} and ${names[2]} like this`,
+// 		4: `${names[0]}, ${names[1]} and ${names.length - 2} others like this`,
+// 	}[Math.min(4, names.length)]
+// }

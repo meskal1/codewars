4dbb3b680aed95cbdf177b8274389efd275a2ccf
Daniil <meskal1@rambler.ru>
2022-03-16 11:16:41 +0300

    Задача_60

--- script.js
+++ script.js
@@ -671,3 +671,17 @@ function century(year) {
 // 	return a
 // }
 // console.log(powersOfTwo(2)); //[1, 2, 4, 8, 16]
+
+// Тимми и Сара думают, что они влюблены, но там, где они живут, они узнают об этом, только когда каждый сорвет по цветку.Если у одного из цветов четное количество лепестков, а у другого нечетное количество лепестков, это означает, что они влюблены.
+// Напишите функцию, которая будет принимать количество лепестков каждого цветка и возвращать true, если они влюблены, и false, если нет.
+// function lovefunc(flower1, flower2) {
+// 	return (flower1 % 2 == 0) && (flower2 % 2 == 0) ? false : !(flower1 % 2 == 0) && !(flower2 % 2 == 0) ? false : true
+// }
+// console.log(lovefunc(1, 4)) // true
+// console.log(lovefunc(2, 2)) // false
+// console.log(lovefunc(0, 1)) // true
+// console.log(lovefunc(0, 0)) // false
+// console.log(lovefunc(307, 867)) // false
+// function lovefunc(flower1, flower2){
+// 	return flower1 % 2 !== flower2 % 2;
+//  }

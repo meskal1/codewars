ec9b9cc1d40ecb8189f4f2db2683376e40b61c79
Daniil <meskal1@rambler.ru>
2022-03-21 00:01:16 +0300

    Задача_76

--- script.js
+++ script.js
@@ -917,3 +917,14 @@ function century(year) {
 // 	return str.split(` `).reverse().join(` `)
 // }
 // console.log(reverseWords("The greatest victory is that which requires no battle"));
+
+// Создайте функцию, которая проверяет, делится ли число n на два числа x И y. Все входные данные являются положительными, ненулевыми цифрами.
+// 1) n =   3, x = 1, y = 3 =>  true because   3 is divisible by 1 and 3
+// 2) n =  12, x = 2, y = 6 =>  true because  12 is divisible by 2 and 6
+// 3) n = 100, x = 5, y = 3 => false because 100 is not divisible by 3
+// 4) n =  12, x = 7, y = 5 => false because  12 is neither divisible by 7 nor 5
+// function isDivisible(n, x, y) {
+// 	return (n % x) || (n % y) ? false : true
+// }
+// console.log(isDivisible(3, 3, 4)); //false
+// console.log(isDivisible(12, 3, 4)); //true

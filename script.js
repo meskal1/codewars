228ff4b4f87fe0008a32e65535afad205f521883
Daniil <meskal1@rambler.ru>
2022-04-25 21:42:35 +0300

    Задача_139

--- script.js
+++ script.js
@@ -1895,3 +1895,23 @@ function century(year) {
 // 	return old >= 21 ? "drink whisky" : old >= 18 ? "drink beer" : old >= 14 ? "drink coke" : 'drink toddy'
 // };
 // console.log(peopleWithAgeDrink(15));
+
+// Если указано число от 0 до 9, верните его словами.
+// Вход :: 1
+// Выход :: "Один".
+// Если ваш язык поддерживает это, попробуйте использовать оператор switch.
+function switchItUp(number) {
+	switch (number) {
+		case 0: return 'Zero'
+		case 1: return 'One'
+		case 2: return 'Two'
+		case 3: return 'Three'
+		case 4: return 'Four'
+		case 5: return 'Five'
+		case 6: return 'Six'
+		case 7: return 'Seven'
+		case 8: return 'Eight'
+		case 9: return 'Nine'
+	}
+}
+console.log(switchItUp(1));

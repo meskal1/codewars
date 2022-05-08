43f9c85099c19e85a67f46d36c46cb13c203fc12
Daniil <meskal1@rambler.ru>
2022-05-08 14:03:28 +0300

    Задача_170

--- script.js
+++ script.js
@@ -2271,3 +2271,21 @@ function century(year) {
 // 		max = Math.max(a, b);
 // 	return (max - min + 1) * (min + max) / 2;
 // }
+
+// Обычно, когда вы что-то покупаете, вас спрашивают, верны ли номер вашей кредитной карты, номер телефона или ответ на ваш самый секретный вопрос. Однако, поскольку кто-то может заглянуть вам через плечо, вы не хотите, чтобы это отображалось на вашем экране. Вместо этого мы маскируем его.
+// Ваша задача — написать функцию maskify, которая заменяет все символы, кроме последних четырех, на «#».
+// Examples
+// "4556364607935616" --> "############5616"
+//      "64607935616" -->      "#######5616"
+//                "1" -->                "1"
+//                 "" -->                 ""
+// // "What was the name of your first pet?"
+// "Skippy" --> "##ippy"
+// "Nananananananananananananananana Batman!" --> "####################################man!"
+// function maskify(cc) {
+// 	return cc.length > 4 ? cc.slice(0, cc.length - 4).replace(/./g, "#") + cc.slice(cc.length - 4, cc.length) : cc
+// }
+// console.log(maskify("4556364607935616"));
+// function maskify(cc) {
+// 	return cc.slice(0, -4).replace(/./g, '#') + cc.slice(-4);
+// }

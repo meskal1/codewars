55753bd8d4ca7209522d8b882367bf92eb6f917d
Daniil <meskal1@rambler.ru>
2022-05-17 18:23:10 +0300

    Задача_188

--- script.js
+++ script.js
@@ -2605,12 +2605,14 @@ function century(year) {
 // console.log(number(["a", "b", "c"]));
 
 // В математике факториал неотрицательного целого числа n, обозначаемый n!, представляет собой произведение всех положительных целых чисел, меньших или равных n.Например: 5! = 5 * 4 * 3 * 2 * 1 = 120. По соглашению значение 0! 1.
-// Напишите функцию для вычисления факториала для заданных входных данных.Если входное значение меньше 0 или больше 12, генерируется исключение типа ArgumentOutOfRangeException(C#), IllegalArgumentException(Java) или RangeException(PHP), либо генерируется RangeError(JavaScript) или ValueError(Python), либо возвращается - 1(C).
+// Напишите функцию для вычисления факториала для заданных входных данных.Если входное значение меньше 0 или больше 12, генерируется RangeError(JavaScript).
 // 	Test.assertEquals(factorial(0), 1, "factorial for 0 is 1");
 // Test.assertEquals(factorial(1), 1, "factorial for 1 is 1");
 // Test.assertEquals(factorial(2), 2, "factorial for 2 is 2");
 // Test.assertEquals(factorial(3), 6, "factorial for 3 is 6");
-function factorial(n) {
-	return n
-}
-console.log(factorial(3));
+// function factorial(n) {
+// 	if (n < 0 || n > 12)
+// 		throw new RangeError();
+// 	return n <= 1 ? 1 : n * factorial(n - 1);
+// }
+// console.log(factorial(0));

b2297c1ee82ba86153030a22d4dd0ea9489bffa7
Daniil <meskal1@rambler.ru>
2022-05-28 15:28:54 +0300

    Задача_204

--- script.js
+++ script.js
@@ -2865,3 +2865,33 @@
 // console.log(checkCoupon("123", "123", "July 9, 2015", "July 9, 2015"));
 // console.log(checkCoupon("123", "123", "July 9, 2015", "July 2, 2015"));
 // console.log(checkCoupon('123', '123', 'September 5, 2014', 'October 1, 2014'));
+
+// Задача
+// Даны делитель и граница. Найдите наибольшее целое число N, такое, что
+// Условия:
+// N делится на делитель
+// N меньше или равно границе
+// N больше 0.
+// Заметки
+// Параметры (делитель, граница), передаваемые в функцию, являются только положительными значениями.
+// Гарантируется, что делитель найден.
+// Input >> Output Examples
+// maxMultiple (2,7) ==> return (6)
+// Explanation:
+// (6) is divisible by (2) , (6) is less than or equal to bound (7) , and (6) is > 0 .
+// maxMultiple (10,50)  ==> return (50)
+// Explanation:
+// (50) is divisible by (10) , (50) is less than or equal to bound (50) , and (50) is > 0 .*
+// maxMultiple (37,200) ==> return (185)
+// Explanation:
+// (185) is divisible by (37) , (185) is less than or equal to bound (200) , and (185) is > 0 .
+// function maxMultiple(divisor, bound) {
+// 	for (let i = bound; i > 0; i--) {
+// 		if (i % divisor === 0) return i
+// 	}
+// }
+// console.log(maxMultiple(2, 7)); // 6
+// console.log(maxMultiple(37, 200)); // 6
+// function maxMultiple(divisor, bound){
+// 	return bound-bound%divisor
+//  }

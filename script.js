7fd9181c2787252d5f3194c21cc54d60ed9e17ce
Daniil <meskal1@rambler.ru>
2022-05-30 10:08:35 +0300

    Задача_208

--- script.js
+++ script.js
@@ -2938,3 +2938,12 @@
 // console.log(smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100)); //false
 // console.log(smallEnough([101, 45, 75, 105, 99, 107], 107)); //true
 // console.log(smallEnough([80, 117, 115, 104, 45, 85, 112, 115], 120)); //true
+// function smallEnough(a, limit){
+// 	return Math.max(...a) <= limit
+//  }
+
+// Найдите сумму внутренних углов (в градусах) n-стороннего простого многоугольника. N будет больше 2.
+// function angle(n) {
+// 	return 180 * (n - 2)
+// }
+// console.log(angle(4)); //360

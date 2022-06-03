36372b23e95754d103d23bec61165470ce015550
Daniil <meskal1@rambler.ru>
2022-06-03 19:05:01 +0300

    Задача_216

--- script.js
+++ script.js
@@ -3081,3 +3081,16 @@
 // 	return a < 0 ? 0 : a
 // }
 // console.log(solution(10)); //23
+
+// Напишите функцию, которая принимает массив из 10 целых чисел (от 0 до 9), которая возвращает строку этих чисел в виде номера телефона.
+// Пример
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => возвращает "(123) 456-7890"
+// Возвращаемый формат должен быть правильным, чтобы выполнить эту задачу.
+// Не забудьте пробел после закрывающей скобки!
+// function createPhoneNumber(numbers) {
+// 	return `(${[...numbers.slice(0, 3)].join(``)}) ${[...numbers.slice(3, 6)].join(``)}-${[...numbers.slice(6)].join(``)}`
+// }
+// console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+// function createPhoneNumber(numbers){
+// 	return numbers.join('').replace(/(...)(...)(.*)/, '($1) $2-$3');
+//  }

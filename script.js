1269194b6a31976dceae6ba68ad4fdd892091735
Daniil <meskal1@rambler.ru>
2022-07-28 07:52:06 +0300

    Задача_248

--- script.js
+++ script.js
@@ -3700,3 +3700,10 @@
 // }
 // console.log(predictAge(65, 60, 75, 55, 60, 63, 64, 45));
 // const predictAge = (...ages) => Math.hypot(...ages) / 2 | 0;
+
+// Учитывая двумерный массив целых чисел, вернуть сглаженную версию массива со всеми целыми числами в отсортированном (по возрастанию) порядке.
+// Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
+// function flattenAndSort(array) {
+// 	return [].concat(...array).sort((a, b) => a - b);
+// }
+// console.log(flattenAndSort([[3, 2, 1], [4, 6, 5], [], [9, 7, 8, 100]]));

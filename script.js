4282907a6b75b854c0cbe6ae76a62e1f317836ed
Daniil <meskal1@rambler.ru>
2022-08-23 22:48:17 +0300

    Задача_264

--- script.js
+++ script.js
@@ -3962,3 +3962,9 @@
 //   return Math.min(...args);
 // }
 // console.log(findSmallestInt([34, -345, -1, 100]));
+
+// Просто удалите пробелы из строки, затем верните результирующую строку.
+// function noSpace(x) {
+//   return x.replace(/ /g, "");
+// }
+// console.log(noSpace("8 j 8   mBliB8g  imjB8B8  jl  B"));
